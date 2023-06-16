8572d1a3076f3ecdee1815787f2e6e078f32e156
psanvicente21 <psanvicente1986@gmail.com>
2023-06-16 10:10:27 +0200

    readded manage.py. Comment line erased

--- polls/tests.py
+++ polls/tests.py
@@ -10,7 +10,6 @@ from django.urls import reverse
 
 from .models import Question
 
-
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
